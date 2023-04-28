837a91d5a54d3f99c85551a397dc79dcda01909c
ahsouza <annibalhsouza@gmail.com>
2023-04-28 15:12:27 -0300

    feat: init resources to scanner port

---
+++ src/libs/info/scan_port.cpp
@@ -0,0 +1,53 @@
+/************************************************************************************
+      
+ *                 Copyright (C) 2021 - 2023, Barca, Inc. 
+ 
+ *    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+ *    Project: BSCAN to scanner MotherBoards. CPU, Memory Ram, SO and more
+ 
+ * This software is licensed as described in the file COPYING, which                    
+ * you should have received as part of this distribution. The terms                     
+ * are also available at https://project-barca.github.io/docs/copyright.html.           
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell             
+ * copies of the Software, and permit persons to whom the Software is                   
+ * furnished to do so, under the terms of the COPYING file.                             
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY            
+ * KIND, either express or implied.                                                      
+ *
+ **************************************************************************************/
+#include <regex>
+#include <string>
+#include <vector>
+
+#if defined(unix) || defined(__unix) || defined(__unix__)
+#elif defined(__APPLE__)
+#elif defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+#include "WMIwrapper.h"
+#pragma comment(lib, "wbemuuid.lib")
+#endif
+
+#include "network/scan_port.h"
+
+namespace bsscan {
+
+  PORT::PORT(const std::string& status, const std::string& service )
+      : _status(status), 
+        _service(service) {}
+
+  std::string& PORT::status(int port) {
+    if (_status.empty()) {
+      _status = getStatus(port);
+    }
+    return _status;
+  }
+
+  std::string& PORT::service(int port) {
+    if (_service.empty()) {
+      _service = getService(port);
+    }
+    return _service;
+  }
+
+};
