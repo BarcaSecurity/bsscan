328ae10deb602e29dd64b199d3dcf9825cd49570
ahsouza <annibalhsouza@gmail.com>
2023-04-28 12:06:01 -0300

    feat: header methods to ports

---
+++ include/network/scan_ports.h
@@ -0,0 +1,45 @@
+/************************************************************************************
+      
+ *                 Copyright (C) 2021 - 2023, Barca, Inc. 
+ 
+ *    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+ *    Project: BSCAN to scanner MotherBoards. CPU, Memory Ram, SO and more
+ 
+ * This software is licensed as described in the file COPYING, which                    
+ * you should have received as part of this distribution. The terms                     
+ * are also available at https://project-barca.github.io/docs/copyright.html.           
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell             
+ * copies of the Software, and permit persons to whom the Software is                   
+ * furnished to do so, under the terms of the COPYING file.                             
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY            
+ * KIND, either express or implied.                                                      
+ *
+ **************************************************************************************/
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace bsscan {
+
+  class PORT {
+   public:
+    PORT() = default;
+    PORT(const std::string& status,
+        const std::string& service)
+    ~PORT() = default;
+
+    std::string& status();
+    std::string& service();
+  
+    static std::string getStatus();
+    static std::string getService();
+
+   private:
+    std::string _status;
+    std::string _service;
+  };
+
+};
